14e178c0bdbda2772de4b3e4b2963c27bcfbbde6
Paul Mougel <mougelpaul@gmail.com>
2012-05-10 13:26:38 +0000

    index.js     - nettoyage du code source pour respecter les standards jquery

--- app/views/rss/index.js
+++ app/views/rss/index.js
@@ -1,23 +1,32 @@
 // Ajouter un article à la liste 
 function add_article(id, titre, contenu) {
-    ($('<div>').html('<p>' + titre + '</p>' + '<div style="display: none">' + contenu + '</div>')
-               .data('id', id)
-    ).appendTo($("#flux_container"));
+    $('#flux_container').append(
+        $('<div>')
+            .data('id', id)
+            .append($('<p>', { html: titre }))
+            .append($('<div>', {
+                html: contenu,
+                style: 'display: none'
+            }))
+    );
 }
 
 // Toggle onclick sur les articles
 function toggle_list_articles() {
-    $("#flux_container div p").unbind('click');
-    $("#flux_container div p").click(function() {
-        $(this).siblings().each(function() {
-            $(this).toggle();
+    $("#flux_container div p")
+        .unbind('click')
+        .click(function() {
+            $(this).siblings().each(function() {
+                $(this).toggle();
+            });
         });
-    });
 }
 
 // Regle le comportement au click sur un flux/dossier
 function click_flux_dossier() {
     $('#liste_flux tr').click(function() {
+
+        // Quand on clique sur un dossier : afficher/cacher ses enfants
         if ($(this).hasClass('dossier')) {
             var elem = $(this).next();
             while (! $(elem).hasClass('dossier')) {
@@ -27,25 +36,26 @@ function click_flux_dossier() {
             return;
         }
 
+        // Sinon, on a cliqué sur un flux : il faut chercher la liste des articles correspondant
         $("#liste_flux tr").removeClass("ligne_flux_selectionne");
-
         $(this).addClass("ligne_flux_selectionne");
 
         // Affichage de la barre de chargement
-        var flux_id = this.id.split('_')[1]; // "flux_ID" : on veut récupérer l'ID uniquement
+        var flux_id = $(this).data('id');
         var flux_titre= $('td:first', $(this)).text();
 
         $("#liste_articles_chargement").slideDown('slow');
         $("#liste_articles_erreur").slideUp('fast');
         
-        $("#flux_container").html("");
         // Recuperation des articles
+        $("#flux_container").html("");
         $.getJSON('../get_articles/' + flux_id, function(data) {
             $.each(data, function(index, elem) {
                 add_article(elem.id, elem.titre, elem.contenu);
             });
             toggle_list_articles();
         })
+
         .success(function() {
             $("#liste_articles_chargement").slideUp('slow');
         })
@@ -61,7 +71,7 @@ $(document).ready(function() {
 
     /* GET LISTE TAGS + BARRE DE RECHERCHE */
     $.get('../get_tags', function(data) {
-        $("#search").attr('data-source', data);
+        $("#search").attr('data-source', data); // Données utilisées pour l'auto-complétion
 
         $('#search').typeahead({
             source: function(typeahead, query) {
@@ -84,12 +94,18 @@ $(document).ready(function() {
             property: "titre",
             onselect: function(obj) {
                 // Ajouter le tag
-                $("<a class='btn btn-primary hide' href='#'>" + obj.titre + " <i class='icon-remove icon-white'></i></a>")
-                    .data('id', obj.id)
-                    .appendTo("#tag-list")
-                    .slideDown('fast');
+                $('<a>', {
+                    class: 'btn btn-primary hide',
+                    href: '#',
+                    html: obj.titre
+                })
+                .append($('<i>', {
+                    class: 'icon-remove icon-white'}))
+                .data('id', obj.id)
+                .appendTo("#tag-list")
+                .slideDown('fast');
 
-                // Ajouter les événements
+                // Ajouter l'événement
                 $("#tag-list a").click(function() {
                     $(this).hide('fast', function() { $(this).remove(); });
                 });
@@ -101,38 +117,39 @@ $(document).ready(function() {
 
     /* GET LISTE DOSSIER + LISTE FLUX */
     $.getJSON('../get_flux_dossiers', function(data) {
-        html_liste_flux = '';
+        
         $.each(data, function(index, dossier) {
             // DOSSIER
-            html_liste_flux += "<tr class='dossier'>";
-            html_liste_flux += "<td colspan='3' style='text-align: center; background-color:#eee' >";
-            html_liste_flux += "<i class='icon-folder-open pull-left'></i> ";
-            html_liste_flux += "<b>" + dossier.titre + "</b>";
-            html_liste_flux += "</td></tr>";
-
+            $('<tr>', { class: 'dossier' })
+                .append($('<td>', {colspan: 3, style: 'text-align: center; background-color: #eee;'})
+                            .append($('<i>', {class: 'icon-folder-open pull-left'}))
+                            .append($('<b>', {html : dossier.titre}))
+                )
+            .appendTo('#liste_flux');
+            
             $.each(dossier.liste_flux, function(index2, flux) {
                 // FLUX
-                html_liste_flux += "<tr id='flux_" + flux.id + "'>";
-                html_liste_flux += "<td>" + flux.titre + "</td>";
-
                 if (flux.nb_nonlus == 0) type_badge = "";
                 else if (flux.nb_nonlus > 0 && flux.nb_nonlus < 10) type_badge = "badge-success";
                 else if (flux.nb_nonlus >= 10 && flux.nb_nonlus <= 50) type_badge = "badge-warning";
                 else type_badge = "badge-important";
 
-                html_liste_flux += '<td><span class="badge ' + type_badge + '">' + flux.nb_nonlus + '</span></td>';
-
-                html_liste_flux += "<td><a href='#'><i class='icon-circle-arrow-right'></i></a></td>";
-
-                html_liste_flux += "</tr>";
+                $('<tr>')
+                    .data('id', flux.id)
+                    .append($('<td>', {html : flux.titre }))
+                    .append($('<td>').append(
+                                $('<span>', {class: 'badge ' + type_badge, html: flux.nb_nonlus})
+                            ))
+                    .append($('<td>')
+                                .append($('<a>', {href: '#'}))
+                                .append($('<i>', {class: 'icon-circle-arrow-right'}))
+                            )
+                .appendTo('#liste_flux')
             });
         });
-        
-        // "Dossier" final invisible, pour que le javascript s'arrete de boucler
-        html_liste_flux += "<tr class='dossier'></tr>";
 
-        // MAJ du DOM
-        $("#liste_flux").html(html_liste_flux);
+        // "Dossier" final invisible, pour que le javascript s'arrete de boucler
+        $('<tr>', {class: 'dossier'}).appendTo('#liste_flux');
 
         // onclick
         click_flux_dossier();
@@ -142,18 +159,20 @@ $(document).ready(function() {
     });
 
      
-    /* GESTION TAGS AUTOCOMPLETE */
+    /* GESTION RECHECHE : on ajoute manuellement les tags comme input caché à la validation */
     $("#form-search").submit(function() {
         var tags_id = [];
         $("#tag-list a").each(function() {
             tags_id.push($(this).data('id'));
         });
 
-        var tag = $("<input>")
-            .attr("type", "hidden")
-            .attr("name", "tags_id")
-            .val(tags_id);
-        $("#form-search").append($(tag));
+
+        $('#form-search').append(
+            $("<input>")
+                .attr("type", "hidden")
+                .attr("name", "tags_id")
+                .val(tags_id)
+        );
         
         return true;
      });
