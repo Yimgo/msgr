7888bd9f0c8d3aac21d7020c123c5e3dd408bcc5
Yimgo <yimgo@yimgo.fr>
2012-05-15 17:29:08 +0000

    * pokemon/lib/ConnectionWrapper.php: 	- signUp() should now be ok 	- checkig query() and execute() return values

--- pokemon/lib/ConnectionWrapper.php
+++ pokemon/lib/ConnectionWrapper.php
@@ -6,14 +6,16 @@ class ConnectionWrapper {
   const sel = 'qsdfhsdherh';
   
   public function __construct($type) {
-    $this->connection=DatabaseConnectionFactory::get($type);
+    $this->connection = DatabaseConnectionFactory::get($type);
   }
   
   public function signIn($login, $pwd) {
-    $statement = $this->connection->prepare('SELECT * FROM user WHERE user_login = :login AND user_password = :pwd;');
+    $statement = $this->connection->prepare('SELECT user_id FROM user WHERE user_login = :login AND user_password = :pwd;');
     $statement->bindParam(':login', $login);
-    $statement->bindParam(':pwd', hash('sha256',$pwd.self::sel));
-    $statement->execute();
+    $statement->bindParam(':pwd', hash('sha256', $pwd.self::sel));
+    if ($statement->execute() === FALSE) {
+      return FALSE;
+    }
     
     if ($user = $statement->fetch()){
 		  return $user["user_id"];
@@ -21,9 +23,12 @@ class ConnectionWrapper {
 		return FALSE;
   }
   
-  public function getTags() {
-    $result = $this->connection->query('SELECT * FROM tag');
-    $tags = array(); 
+  public function getTags() { 
+    if (($result = $this->connection->query('SELECT * FROM tag')) === FALSE) {
+      return array();
+    }
+    
+    $tags = array();
     while ($row = $result->fetch()) {
         array_push($tags, array("titre" => $row["tag_nom"], "id" => $row["tag_id"]));
     }
@@ -31,16 +36,15 @@ class ConnectionWrapper {
   }
   
   public function signUp($login, $pwd, $email) {
-    $statement = $this->connection->prepare('INSERT INTO user(user_login, user_password, user_email) VALUES(:login, :pwd, :email);');
-    $statement->bindParam(':login', $login);
-    $statement->bindParam(':pwd', $pwd);
-    $statement->bindParam(':email', $email);
-    $statement->execute();
+    $insert_statement = $this->connection->prepare('INSERT INTO user(user_login, user_password, user_email) VALUES(:login, :pwd, :email);');
+    $insserert_statement->bindParam(':login', $login);
+    $insert_statement->bindParam(':pwd', hash('sha256', $pwd.self::sel));
+    $insert_statement->bindParam(':email', $email);
     
-    print_r($statement->fetchAll());
-    
-    return FALSE;
-  }
-    
+    if($statement->execute() === FALSE) {
+      return FALSE;
+    }
+    return $this->signIn($login, $pwd);
+  }    
 }
 ?>
