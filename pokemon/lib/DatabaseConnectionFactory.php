607bd431a060ef41e3c70370e57b0d0f7664bb58
Yimgo <yimgo@yimgo.fr>
2012-05-17 18:13:23 +0000

    Introducing new way to manage database connexion. Static configuration (not only for db) is now located in lib/config.ini, where is set default_profile parameter. User can add the override.ini file to override configuration located in lib/config.ini, if debug is set to 1. An example is available: lib/override.ini.example. Profiles are located in lib/db_profiles, with one file per profile. User can add his own profiles.

--- pokemon/lib/DatabaseConnectionFactory.php
+++ pokemon/lib/DatabaseConnectionFactory.php
@@ -2,10 +2,40 @@
   require_once 'lib/PDOConnection.php';
   
 class DatabaseConnectionFactory {
-  public static function get($type) {
-    require_once('lib/databaseConnectionProfiles.php');
-    $getParams = 'get'.$type.'Params';
-    return PDOConnection::getInstance($getParams());
+  public static function get($profile) {
+    $params = array();
+    
+    if(!file_exists('lib/db_profiles/'.$profile.'.ini')) {
+      return FALSE;
+    }
+    
+    $config = parse_ini_file('lib/db_profiles/'.$profile.'.ini', true);
+
+    $params['dsn'] = self::getDSN($config['General']['driver'], $config['General']['host'], $config['General']['port'], $config['General']['dbname']);
+    $params['username'] = $config['General']['username'];
+    $params['password'] = $config['General']['password'];
+    if(isset($config['DriverOptions'])) {
+      $params['driver_options'] = self::getDriverOptions($config['DriverOptions']);
+    }
+    else {
+      $params['driver_options']=array();
+    }
+    
+    return PDOConnection::getInstance($params);
+  }
+  
+  public static function getDSN($driver, $host, $port, $dbname) {
+    return $driver.':host='.$host.';port='.$port.';dbname='.$dbname;
+  }
+  
+  public static function getDriverOptions($config) {
+    $optionsArray = array();
+    $pdoClass = new ReflectionClass('PDO');
+    foreach($config as $key => $value) {
+      $optionsArray[$pdoClass->getConstant($key)] = $value;
+    }
+    
+    return $optionsArray;
   }
 }
 ?>
